9fe0c3f4f6bde18a46514a3ea4dfc1e21fac3b59
rudi schenck <rudische@gmail.com>
2019-01-14 23:42:38 -0500

    User data model, home page photo links

---
+++ mgPhotosMVC/Models/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace mgPhotosMVC.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public MailAddress Address { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string Email { get; set; }
+    }
+}
